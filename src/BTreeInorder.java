d5466809b2e210c8f20c7d47d53c6382b5f64f22
amysue <staramy_2005@126.com>
2015-09-21 19:22:37 +0800

    create

---
+++ src/BTreeInorder.java
@@ -0,0 +1,13 @@
+import java.util.List;
+public class BTreeInorder {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x;}
+    }
+    private List<Integer> bTreeList;
+    public List<Integer> inorderTraversal(TreeNode root) {
+        return bTreeList;
+    }
+}
