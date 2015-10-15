afd64c6c55c5208efa1f8fcad131136ff0430d7a
amykiki <staramy_2005@126.com>
2015-10-15 10:41:52 +0800

    format change

--- src/RemDupList.java
+++ src/RemDupList.java
@@ -4,15 +4,15 @@ public class RemDupList {
         ListNode pHead = head;
         ListNode pNext = head.next;
         while (pNext != null) {
-        	if (pNext.val == pHead.val) {
-        		while (pNext != null && pNext.val == pHead.val) {
-        			pNext = pNext.next;
-        		}
-        		pHead.next = pNext;
-        		if (pNext == null) break;
-        	}
-        	pHead = pNext;
-        	pNext = pNext.next;
+            if (pNext.val == pHead.val) {
+                while (pNext != null && pNext.val == pHead.val) {
+                    pNext = pNext.next;
+                }
+                pHead.next = pNext;
+                if (pNext == null) break;
+            }
+            pHead = pNext;
+            pNext = pNext.next;
         }
         return head;
     }
