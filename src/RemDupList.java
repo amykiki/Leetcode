3ffe9a7dfad8e460103734b7d2c8e3e7825c61b4
amykiki <staramy_2005@126.com>
2015-10-15 10:35:20 +0800

    update #83

--- src/RemDupList.java
+++ src/RemDupList.java
@@ -1,17 +1,19 @@
-
 public class RemDupList {
     public ListNode deleteDuplicates(ListNode head) {
         if (head == null || head.next == null) return head;
         ListNode pHead = head;
         ListNode pNext = head.next;
         while (pNext != null) {
-            if (pNext.val != pHead.val) {
-                pHead.next = pNext;
-                pHead = pNext;
-            }
-            pNext = pNext.next;
+        	if (pNext.val == pHead.val) {
+        		while (pNext != null && pNext.val == pHead.val) {
+        			pNext = pNext.next;
+        		}
+        		pHead.next = pNext;
+        		if (pNext == null) break;
+        	}
+        	pHead = pNext;
+        	pNext = pNext.next;
         }
-        pHead.next = null;
         return head;
     }
 }
