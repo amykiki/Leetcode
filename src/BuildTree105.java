9d4fcce6a74193feedde074b4aa859cd47647226
amykiki <staramy_2005@126.com>
2016-07-21 16:53:14 +0800

    problem 105, run time 5ms, beats 83.24%

--- src/BuildTree105.java
+++ src/BuildTree105.java
@@ -1,20 +1,58 @@
 import java.util.List;
+import java.util.Stack;
 
 /**
  * Created by Amysue on 2016/7/18.
  */
 public class BuildTree105 {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        TreeNode root = new TreeNode(preorder[0]);
-
+        if (preorder.length == 0) {
+            return null;
+        }
+        TreeNode        root     = new TreeNode(preorder[0]);
+        Stack<TreeNode> midNodes = new Stack<>();
+        midNodes.push(root);
+        TreeNode lastNode = null;
+        TreeNode curNode;
+        int      i        = 0;
+        int      j        = 1;
+        while (j < preorder.length) {
+            if (!midNodes.isEmpty() && inorder[i] == midNodes.peek().val) {
+                lastNode = midNodes.pop();
+                i++;
+            } else {
+                curNode = new TreeNode(preorder[j]);
+                if (lastNode != null) {
+                    lastNode.right = curNode;
+                    lastNode = null;
+                } else {
+                    midNodes.peek().left = curNode;
+                }
+                if (inorder[i] == preorder[j]) {
+                    lastNode = curNode;
+                    i++;
+                } else {
+                    midNodes.push(curNode);
+                }
+                j++;
+            }
+        }
         return root;
     }
 
     public static void main(String[] args) {
         BuildTree105  buildTree105 = new BuildTree105();
-        int[]         inorder1     = {2, 3,4,5,7,8,9,10,12,13,14,15,17,19,18};
-        int[]         preorder1    = {10,5,3,2,4,8,7,9, 15,13,12,14,18,17,19};
-        TreeNode      root         = buildTree105.buildTree(inorder1, preorder1);
+        int[]         inorder1     = {2, 3, 4, 5, 7, 8, 9, 10, 12, 13, 14, 15, 17, 18, 19};
+        int[]         preorder1    = {10, 5, 3, 2, 4, 8, 7, 9, 15, 13, 12, 14, 18, 17, 19};
+//        int[]         inorder2     = {3,2,1,5,4,6};
+//        int[]         preorder2    = {1,2,3,4,5,6};
+//        int[]         inorder3     = {6,5,4,3,2,1};
+//        int[]         preorder3    = {1,2,3,4,5,6};
+        /*int[]         inorder4     = {4,2,5,3,6,1};
+        int[]         preorder4    = {1,2,4,3,5,6};*/
+        int[]         inorder5     = {2,4,6,5,3,1};
+        int[]         preorder5    = {1,2,3,4,5,6,};
+        TreeNode      root         = buildTree105.buildTree(preorder1, inorder1);
         BinaryTree    bt           = new BinaryTree(root);
         List<Integer> list         = bt.inorder();
         System.out.println(list);
