a7f0bcbe1fc9e8b42c1252e3f311e7f508de4f84
amykiki <staramy_2005@126.com>
2016-07-26 17:26:43 +0800

    Convert Sorted List to Binary Search Tree-109 run time 2ms/21.28%, beats 7.3%，不是最优解，需看看其他的办法

---
+++ src/SortedListToBST109.java
@@ -0,0 +1,39 @@
+/**
+ * Created by Amysue on 2016/7/26.
+ */
+public class SortedListToBST109 {
+    public TreeNode sortedListToBST(ListNode head) {
+        TreeNode root = findMiddle(head, null);
+        return root;
+    }
+
+    private TreeNode findMiddle(ListNode low, ListNode high) {
+        if (low == null || low == high) {
+            return null;
+        }
+        ListNode slow = low;
+        ListNode fast = low;
+        while (fast != high) {
+            fast = fast.next;
+            if (fast == null || fast == high) {
+                break;
+            }
+            fast = fast.next;
+            slow = slow.next;
+        }
+        TreeNode head = new TreeNode(slow.val);
+        TreeNode leftNode = findMiddle(low, slow);
+        TreeNode rightNode = findMiddle(slow.next, high);
+        head.left = leftNode;
+        head.right = rightNode;
+        return head;
+    }
+
+    public static void main(String[] args) {
+        SortedListToBST109 st = new SortedListToBST109();
+        int[] nums = {1,2,3,4,5,6,7,8,9};
+        ListNode head = ListNode.genNodes(nums);
+        TreeNode root = st.sortedListToBST(head);
+        System.out.println(1);
+    }
+}
