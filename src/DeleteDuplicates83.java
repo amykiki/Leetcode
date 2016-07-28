eba208ebd6b94e72342bf1e462c644beb9cb8b89
amykiki <staramy_2005@126.com>
2016-07-28 16:47:28 +0800

    Remove Duplicates from Sorted List-83, run time 1ms/82.85%, beats 17.15%

---
+++ src/DeleteDuplicates83.java
@@ -0,0 +1,35 @@
+/**
+ * Created by Amysue on 2016/7/28.
+ */
+public class DeleteDuplicates83 {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode p,q;
+        p = head;
+        q = head.next;
+        while (q != null) {
+            while (q != null && p.val == q.val) {
+                q = q.next;
+            }
+            p.next = q;
+            p = q;
+            if (q != null) {
+                q = q.next;
+            }
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        DeleteDuplicates83 dd   = new DeleteDuplicates83();
+//        int[]              nums = {1, 2, 2, 2, 2, 3, 4, 5, 5, 5, 5, 6, 7, 8, 8, 9, 10};
+        int[]              nums = {5,5};
+        ListNode head = ListNode.genNodes(nums);
+        ListNode.printNods(head);
+        head = dd.deleteDuplicates(head);
+        ListNode.printNods(head);
+
+    }
+}
