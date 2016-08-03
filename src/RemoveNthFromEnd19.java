bb2306b2557c83144a91a9ea3502060c18dad399
amykiki <staramy_2005@126.com>
2016-08-03 18:41:23 +0800

    Remove Nth Node From End of List-19, 虽然运行速率没有提高，但是整个逻辑变了，这个更加流畅，采用两个指针，一个先走n步，一个后走

--- src/RemoveNthFromEnd19.java
+++ src/RemoveNthFromEnd19.java
@@ -9,40 +9,22 @@ public class RemoveNthFromEnd19 {
         if (head == null || head.next == null) {
             return null;
         }
-        ListNode slow = head;
-        ListNode fast = head.next;
-        int count = 1;
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-            count++;
-        }
-        int sum = 0;
-        if (fast == null) {
-            sum = 2*count -1;
-        } else {
-            sum = 2*count;
-        }
-        int k = sum - n;
-        if (k == 0) {
-            return head.next;
-        }
-        ListNode p, q;
-        if (k >= count) {
-            k = k - count;
-            p = slow;
-        } else {
-            p = new ListNode(0);
-            p.next = head;
-        }
+        ListNode h,p,q;
+        h = new ListNode(0);
+        h.next = head;
+        p = h;
+        q = h;
         int i = 0;
-        while (i < k) {
-            p = p.next;
+        while (i < n) {
+            q = q.next;
             i++;
         }
+        while (q.next != null) {
+            p = p.next;
+            q = q.next;
+        }
         p.next = p.next.next;
-
-        return head;
+        return h.next;
     }
 
     public static void main(String[] args) {
@@ -50,8 +32,7 @@ public class RemoveNthFromEnd19 {
         int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         ListNode head = ListNode.genNodes(nums);
         ListNode.printNods(head);
-        int n = 3;
-        head = rn.removeNthFromEnd(head, 4);
+        head = rn.removeNthFromEnd(head, 1);
         ListNode.printNods(head);
     }
 }
