efdb1b6eb551a77784c8baca156c8192efda91cd
amykiki <staramy_2005@126.com>
2016-08-13 15:37:13 +0800

    First Missing Positive41, run time 1ms/81.81%, beats 18.19%

---
+++ src/FirstMissingPositive41.java
@@ -0,0 +1,43 @@
+/**
+ * Created by Amysue on 2016/8/13.
+ */
+public class FirstMissingPositive41 {
+    public int firstMissingPositive(int[] nums) {
+        int N = nums.length;
+        for(int i = 0; i < nums.length; i++) {
+            if (nums[i] == i + 1) {
+                continue;
+            }
+            if (nums[i] <= 0 || nums[i] > N) {
+                continue;
+            }
+            int m = nums[i];
+            nums[i] = 0;
+            swap(nums, m, m - 1);
+        }
+        for(int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                return i+1;
+            }
+        }
+        return nums.length + 1;
+    }
+
+    private void swap(int[] nums, int m, int j) {
+        int tmp = nums[j];
+        nums[j] = m;
+        if (tmp == j + 1 || tmp <= 0 || tmp > nums.length) {
+            return;
+        }
+        swap (nums,tmp, tmp - 1);
+
+    }
+
+    public static void main(String[] args) {
+        FirstMissingPositive41 fm = new FirstMissingPositive41();
+//        int[] nums = {3, 7, 4, 6, 2, 1, 7, 6, -2};
+        int[] nums = {3,4,-1,1};
+        int first = fm.firstMissingPositive(nums);
+        System.out.println("first = " + first);
+    }
+}
