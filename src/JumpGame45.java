d2cd55034bd9d54ba53e2ef7b8089e13c1e1a126
amykiki <staramy_2005@126.com>
2016-08-17 19:31:08 +0800

    jump game45, run time 3ms/37.32%, beats 55.11%

--- src/JumpGame45.java
+++ src/JumpGame45.java
@@ -1,5 +1,3 @@
-import java.util.LinkedList;
-
 /**
  * Created by Amysue on 2016/8/17.
  */
@@ -8,37 +6,44 @@ public class JumpGame45 {
         if (nums == null || nums.length <= 1) {
             return 0;
         }
-        LinkedList<Integer> nodes = new LinkedList<>();
-        nodes.add(0);
+        int[] ides = new int[nums.length];
+        ides[0] = 0;
         boolean[] marked = new boolean[nums.length];
         marked[0] = true;
-        int N = nums.length - 1;
+        int N     = nums.length - 1;
         int level = 0;
-        while (!nodes.isEmpty()) {
-            int count = nodes.size();
+        int count = 1;
+        int j     = 0;
+        while (true) {
+            int n         = j + count;
+            int nextCount = 0;
             while (count > 0) {
-                int j = nodes.removeFirst();
-                for(int i = nums[j]; i > 0; i--) {
-                    if (j + i >= N) {
+                int numi = ides[j];
+                for (int i = nums[numi]; i > 0; i--) {
+                    if (numi + i >= N) {
                         return ++level;
                     }
-                    if (!marked[j + i]) {
-                        marked[j+i] = true;
-                        nodes.add(j+i);
+                    if (!marked[numi + i]) {
+                        marked[numi + i] = true;
+                        ides[n++] = numi + i;
+                        nextCount++;
+                    } else {
+                        break;
                     }
                 }
                 count--;
+                j++;
             }
+            count = nextCount;
             level++;
         }
-        return 0;
     }
 
     public static void main(String[] args) {
         JumpGame45 jg = new JumpGame45();
-//        int[] nums = {2,3,1,1,4};
-        int[] nums = {3, 4, 3, 2, 5, 4, 3};
-        int result = jg.jump(nums);
+//        int[]      nums = {2, 3, 1, 1, 4};
+        int[] nums   = {3, 4, 3, 2, 5, 4, 3};
+        int   result = jg.jump(nums);
         System.out.println("result = " + result);
     }
 
