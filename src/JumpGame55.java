b7ccde044dacb896b784af165382f9f0cd299a3d
amykiki <staramy_2005@126.com>
2016-08-25 21:47:22 +0800

    Jump Game-55, run time 2ms/22.53%, beats 72.72%, 相比第一次的做法，总体思路是没有改变的，但是第一次的做法，很累赘，添加了很多不该加的，导致运行速度慢

--- src/JumpGame55.java
+++ src/JumpGame55.java
@@ -4,51 +4,57 @@ import java.util.*;
  * Created by Amysue on 2016/8/25.
  */
 public class JumpGame55 {
+
     public boolean canJump(int[] nums) {
         if (nums == null || nums.length <= 1) {
             return true;
         }
-        LinkedList<Integer> queue = new LinkedList<>();
-        LinkedList<Integer> list = new LinkedList<>();
-        queue.push(0);
-        while (!queue.isEmpty()) {
-            if (jump(nums, queue, list)) {
-                return true;
-            }
-            queue = list;
-            list = new LinkedList<>();
+        int N = nums.length - 1;
+        int[] results = new int[2];
+        results[0] = 0;
+        results[1] = nums[0];
+        if (nums[0] >= N) {
+            return true;
         }
-
-        return false;
+        int rc = 0;
+        while (true) {
+            rc = jump(nums, results, N);
+            if (rc != 0) {
+                break;
+            }
+        }
+        return rc > 0 ? true : false;
     }
 
-    private boolean jump(int[] nums, LinkedList<Integer> queue, LinkedList<Integer> list) {
-        int                 max  = queue.peek();
-        while (!queue.isEmpty()) {
-            int i     = queue.pop();
+    private int jump(int[] nums, int[] results, int N) {
+        int low = results[0];
+        int high = results[1];
+        int                 max  = high;
+        for(int i = high; i > low; i--) {
             int maxI  = nums[i] + i;
-            if (maxI >= nums.length - 1) {
-                return true;
+            if (maxI >= N) {
+                return 1;
             }
-            int count = maxI - max;
-            if (count > 0) {
+            if (maxI > max) {
                 max = maxI;
-                int j = i + 1 + nums[i] - count;
-                while (j <= maxI) {
-                    list.push(j);
-                    j++;
-                }
             }
         }
-        return false;
+        if (max == high) {
+            return -1;
+        } else {
+            results[0] = high;
+            results[1] = max;
+            return 0;
+        }
     }
 
     public static void main(String[] args) {
         JumpGame55 jg   = new JumpGame55();
 //        int[]      nums = {3, 2, 1, 0, 4};
 //        int[]      nums = {2,3,1,1,4};
-//        int[]      nums = {2, 0, 3, 4, 4, 2, 3, 1, 4, 0};
-        int[]      nums = {3, 5, 1, 1, 1, 5, 0, 3, 4, 2};
+        int[]      nums = {2, 0, 3, 4, 4, 2, 3, 1, 4, 0};
+//        int[]      nums = {2, 0};
+//        int[]      nums = {3, 5, 1, 1, 1, 5, 0, 3, 4, 2};
         System.out.println(jg.canJump(nums));
         /*Random rand  = new Random();
         int    n     = 10;
