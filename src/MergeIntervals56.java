889060e0cceb75c6cf2198af641c2f38a2f0b074
amykiki <staramy_2005@126.com>
2016-08-26 19:51:32 +0800

    Merge Intervals-56, improve run time 2ms, run time 13ms/4.29%, beats 89.09%

--- src/MergeIntervals56.java
+++ src/MergeIntervals56.java
@@ -11,11 +11,6 @@ class IntervalComparator implements Comparator<Interval> {
         } else if (o1.end > o2.end) {
             return 1;
         }
-        if (o1.start < o2.start) {
-            return -1;
-        } else if (o1.start > o2.start) {
-            return 1;
-        }
         return 0;
     }
 }
@@ -32,19 +27,22 @@ public class MergeIntervals56 {
         Collections.sort(intervals, cp);
         LinkedList<Interval> result = new LinkedList<>();
         Interval crt = intervals.get(N);
+        int high = crt.end;
+        int low = crt.start;
         for(int i = N-1; i>= 0; i--) {
             Interval iter = intervals.get(i);
-            if (iter.end < crt.start) {
-                result.push(crt);
-                crt = iter;
+            if (iter.end < low) {
+                result.push(new Interval(low, high));
+                low = iter.start;
+                high = iter.end;
             } else {
-                if (iter.start < crt.start) {
-                    crt = new Interval(iter.start, crt.end);
+                if (iter.start < low) {
+                    low = iter.start;
                 }
             }
 
         }
-        result.push(crt);
+        result.push(new Interval(low, high));
         return result;
     }
 
@@ -53,10 +51,10 @@ public class MergeIntervals56 {
         List<Interval> intervals = new ArrayList<>();
 //        int[] starts = {1, 2, 10, 15, 7};
 //        int[] ends = {3, 8, 13, 18, 11};
-//        int[] starts = {1, 2, 8,15};
-//        int[] ends = {3,6,10,18};
-        int[] starts = {1, 1,1,1};
-        int[] ends = {2,2,2,2};
+        int[] starts = {1, 2, 8,15};
+        int[] ends = {3,6,10,18};
+//        int[] starts = {1, 1,1,1};
+//        int[] ends = {2,2,2,2};
        /* intervals.add(new Interval(1, 3));
         intervals.add(new Interval(2, 8));
         intervals.add(new Interval(8, 10));
