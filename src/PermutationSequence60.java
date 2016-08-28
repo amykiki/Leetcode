dfecbf94f58c23bd1ce825b5609198426bc5b217
amykiki <staramy_2005@126.com>
2016-08-28 18:59:39 +0800

    Permutation Sequence-60, run time 3ms/47.65%, beats 26.31%, 用数组替代hashmap，提高1ms。正常运行速度

--- src/PermutationSequence60.java
+++ src/PermutationSequence60.java
@@ -4,12 +4,13 @@ import java.util.*;
  * Created by Amysue on 2016/8/28.
  */
 public class PermutationSequence60 {
-    private static Map<Integer, Integer> fac = computeFac();
-    private static Map<Integer, Integer> computeFac() {
-        Map<Integer, Integer> fac = new HashMap<>(8);
-        fac.put(1, 1);
+    private static int[] fac = computeFac();
+    private static int[] computeFac() {
+        int[] fac = new int[9];
+        fac[0] = 1;
+        fac[1] = 1;
         for(int i = 2; i <= 8; i++) {
-            fac.put(i, i * fac.get(i - 1));
+            fac[i] = i * fac[i-1];
         }
         return fac;
     }
@@ -20,19 +21,19 @@ public class PermutationSequence60 {
             char c = (char) (i + '0');
             list.add(c);
         }
-        fill(n, k, nums, 0, list, fac);
+        fill(n, k, nums, 0, list);
         return new String(nums);
     }
-    private void fill(int n, int k, char[] nums, int i, List<Character> list, Map<Integer, Integer> fac) {
+    private void fill(int n, int k, char[] nums, int i, List<Character> list) {
         if (n == 1) {
             nums[i] = list.get(0);
             return;
         }
-        int fnum = fac.get(n-1);
+        int fnum = fac[n-1];
         int index = (k-1)/ fnum;
         nums[i] = list.get(index);
         list.remove(index);
-        fill(n - 1, k - index*fnum, nums, i+1, list, fac);
+        fill(n - 1, k - index*fnum, nums, i+1, list);
     }
 
     public static void main(String[] args) {
