c622e2d2d55d89b5473c7192f2a0743ecf9b476a
amykiki <staramy_2005@126.com>
2016-08-28 18:46:28 +0800

    Permutation Sequence-60, run time 4ms/22.60%, beats 3.71%

--- src/PermutationSequence60.java
+++ src/PermutationSequence60.java
@@ -4,45 +4,41 @@ import java.util.*;
  * Created by Amysue on 2016/8/28.
  */
 public class PermutationSequence60 {
-    public String getPermutation(int n, int k) {
-        StringBuilder nums = new StringBuilder(n);
-        List<Integer> list = new ArrayList<>(n);
-        for(int i = 1; i <= n; i++) {
-            list.add(i);
+    private static Map<Integer, Integer> fac = computeFac();
+    private static Map<Integer, Integer> computeFac() {
+        Map<Integer, Integer> fac = new HashMap<>(8);
+        fac.put(1, 1);
+        for(int i = 2; i <= 8; i++) {
+            fac.put(i, i * fac.get(i - 1));
         }
-        Map<Integer, Integer> fac = new HashMap<>(n);
-        computeFac(n - 1, fac);
-        fill(n, k, nums, list, fac);
-        return nums.toString();
+        return fac;
     }
-
-    private void fill(int n, int k, StringBuilder nums, List<Integer> list, Map<Integer, Integer> fac) {
+    public String getPermutation(int n, int k) {
+        char[] nums = new char[n];
+        List<Character> list = new ArrayList<>(n);
+        for(int i = 1; i <= n; i++) {
+            char c = (char) (i + '0');
+            list.add(c);
+        }
+        fill(n, k, nums, 0, list, fac);
+        return new String(nums);
+    }
+    private void fill(int n, int k, char[] nums, int i, List<Character> list, Map<Integer, Integer> fac) {
         if (n == 1) {
-            nums.append(list.get(0));
+            nums[i] = list.get(0);
             return;
         }
         int fnum = fac.get(n-1);
         int index = (k-1)/ fnum;
-        int i = list.get(index);
-        nums.append(i);
+        nums[i] = list.get(index);
         list.remove(index);
-        fill(n - 1, k - index*fnum, nums, list, fac);
-    }
-    private void computeFac(int n, Map<Integer, Integer> fac) {
-        fac.put(0, 1);
-        fac.put(1, 1);
-        if (n <= 1) {
-            return;
-        }
-        for(int i = 2; i <= n; i++) {
-            fac.put(i, i * fac.get(i - 1));
-        }
+        fill(n - 1, k - index*fnum, nums, i+1, list, fac);
     }
 
     public static void main(String[] args) {
         PermutationSequence60 ps = new PermutationSequence60();
         int n = 4;
-        int k = 5;
+        int k = 6;
         String s = ps.getPermutation(n, k);
         System.out.println("s = " + s);
 
