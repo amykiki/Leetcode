0d5523fc8cd852dc4c778e65012c397f474bad6d
amykiki <staramy_2005@126.com>
2016-09-01 00:03:39 +0800

    Minimum Path Sum-64, 没有用额外的空间，节省1ms，这其实就是个DP的case，我一开始想复杂，想用最短路径了

--- src/MinPathSum64.java
+++ src/MinPathSum64.java
@@ -8,24 +8,22 @@ public class MinPathSum64 {
     public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int[][] datas = new int[m][n];
         int maxX = m-1;
         int maxY = n-1;
-        datas[0][0] = grid[0][0];
-        datas[maxX][maxY] = grid[maxX][maxY];
+
         for(int j = n-2; j >= 0; j--) {
-            datas[maxX][j] = grid[maxX][j] + datas[maxX][j + 1];
+            grid[maxX][j] = grid[maxX][j] + grid[maxX][j + 1];
         }
         for(int i = m-2; i >= 0; i--) {
-            datas[i][maxY] = grid[i][maxY] + datas[i + 1][maxY];
+            grid[i][maxY] = grid[i][maxY] + grid[i + 1][maxY];
         }
 
         for(int j = n-2; j >= 0; j--) {
             for(int i = m-2; i >= 0; i--) {
-                datas[i][j] = grid[i][j] + Math.min(datas[i+1][j], datas[i][j+1]);
+                grid[i][j] = grid[i][j] + Math.min(grid[i+1][j], grid[i][j+1]);
             }
         }
-        return datas[0][0];
+        return grid[0][0];
     }
 
     public static void main(String[] args) {
