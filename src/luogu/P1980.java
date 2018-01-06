b2f77948c680d455f3e736ed03efa0034945b644
amykiki <staramy_2005@126.com>
2018-01-07 01:56:01 +0800

    p1980还需要完善，通过8个测试点

--- src/luogu/P1980.java
+++ src/luogu/P1980.java
@@ -6,9 +6,6 @@ import java.util.Scanner;
 
 public class P1980 {
     public static void main(String[] args) {
-//        String data = "1000000 2";
-//            String data = "9876543 2";
-//        Scanner cin = new Scanner(data);
         Scanner cin=new Scanner(System.in);
         int N = cin.nextInt(), x = cin.nextInt();
         int length = 1;
@@ -16,49 +13,54 @@ public class P1980 {
         while ((tempN = tempN / 10) != 0) {
             length++;
         }
-        if (length == 1) {
-            if (x == 0) {
-                System.out.println(0);
-            }
-            System.out.println(1);
-        }
         int[] nums = new int[length];
         int[] steps = new int[length];
         int[] results = new int[length];
         int step = 1;
+        tempN = N;
         for(int i = 0; i < length; i++) {
+            nums[i] = tempN % 10;
+            tempN = tempN/10;
             steps[i] = step;
             step *= 10;
         }
+//        CommonUtil.printArray(nums);
+//        CommonUtil.printArray(steps);
         tempN = N;
-        for(int i = length - 1; i >= 0; i--) {
-            nums[i] = tempN / steps[i];
-            tempN = tempN % steps[i];
+        int last = 0;
+        for(int i = 0; i < length; i++) {
+            int r1 = tempN % 10;
+            tempN = tempN / 10;
+            int r2 = tempN;
+            int count;
+            int remain = 0;
+            if (x == 0) {
+                count = r2;
+            }else {
+                if (r1 > x) {
+                    count = 1 ;
+                } else if (r1 < x) {
+                    count = 0;
+                }else {
+                    count = 0;
+                    if (i == 0) {
+                        remain = 1;
+                    }else {
+                        remain = last + 1;
+                    }
+                }
+                if (r2 > 0) {
+                    count += r2;
+                }
+            }
+            results[i] = count * steps[i] + remain;
+            last += r1*steps[i];
         }
-        CommonUtil.printArray(nums);
-        CommonUtil.printArray(steps);
-        //计算最高位
-        int left = N - nums[length - 1] * steps[length - 1];
-        if (nums[length - 1] < x) {
-            results[length - 1] = 0;
-        } else if (nums[length - 1] == x) {
-            results[length - 1] = left + 1;
-        } else {
-            results[length - 1] = steps[length - 1];
-        }
-        for(int i = 0; i < length - 1; i++) {
-            int result = N / steps[i];
-            int r1 = result / 10;
-            int r2 = result % 10;
-            results[i] = r2 >= x ? (r1 + 1) * steps[i] : r1 * steps[i];
-        }
-        CommonUtil.printArray(results);
+//        CommonUtil.printArray(results);
         int sum = 0;
         for(int i = 0; i < length; i++) {
             sum += results[i];
         }
         System.out.println(sum);
     }
-
-
 }
